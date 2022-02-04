74dd4787932dbbb9e21d1c776f6cf23b09d892d2
pilillo <>
2022-02-04 18:06:51 +0100

    fixed compose and changed css file

--- start-osrm.sh
+++ start-osrm.sh
@@ -1,23 +1,47 @@
-#! /bin/bash
+#! /bin/sh
 
 set -e
 
+OSM_FILE="/data/map.osm.pbf"
+OSRM_FILE="/data/map.osrm"
 
-if [ "$1" = "run" ]; then
-    until test -f "/data/map.osrm"; do
-        >&2 echo "Waiting for map osrm - sleeping"
+OSM_FILE_LOCK="/data/osm.lock"
+OSRM_FILE_LOCK="/data/osrm.lock"
+
+if [ "$1" = "download" ]; then
+    if [[ ! -f "$OSM_FILE" ]]; then
+        echo "No local map file available. Downloading from ${MAP_URL}"
+        touch ${OSM_FILE_LOCK}
+        curl ${MAP_URL} --output "${OSM_FILE}"
+        rm ${OSM_FILE_LOCK}
+    fi
+elif [ "$1" = "run" ]; then
+    until test -f "${OSRM_FILE}" && test ! -f "${OSRM_FILE_LOCK}"; do
+        >&2 echo $(ls "${OSRM_FILE}")
+        >&2 echo $(ls "${OSRM_FILE_LOCK}")
+        >&2 echo "Waiting for map osrm to be available - sleeping"
         sleep 3
     done
     echo "Running OSRM"
-    osrm-routed --algorithm mld /data/map.osrm
+    osrm-routed --algorithm mld ${OSRM_FILE}
 else
-    until test -f "/data/map.osm.pbf"; do
-        >&2 echo "Waiting for map pbf - sleeping"
+    # waiting for map file
+    until test -f "${OSM_FILE}" && test ! -f "${OSM_FILE_LOCK}"; do
+        >&2 echo "Waiting for map pbf to be available - sleeping"
         sleep 3
     done
-    echo "Extracting OSRM file"
-    osrm-extract -p /opt/car.lua /data/map.osm.pbf
-    osrm-partition /data/map.osrm
-    osrm-customize /data/map.osrm
-    #osrm-extract /data/map.osm.pbf
+
+    # extract any time newer than yesterday
+    refdate=$(date -d "yesterday" +%s)
+    filedate=$(stat -c "%W" "${OSM_FILE}")
+    if [ ! -f "${OSRM_FILE}" ] || [ "$filedate" -gt "$refdate" ]; then
+        echo "Extracting OSRM file"
+        # lock via a lock file
+        touch "${OSRM_FILE_LOCK}"
+        osrm-extract -p /opt/car.lua ${OSM_FILE}
+        #osrm-extract ${OSM_FILE}
+        osrm-partition ${OSRM_FILE}
+        osrm-customize ${OSRM_FILE}
+        rm "${OSRM_FILE_LOCK}"
+    fi
 fi
